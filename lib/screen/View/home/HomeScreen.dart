7eee11655ccf30415c920103741a612261892042
Avesh6754 <Aweshprajapati150@gmail.com>
2024-09-15 10:07:25 +0530

    15-09-2024

--- lib/screen/View/home/HomeScreen.dart
+++ lib/screen/View/home/HomeScreen.dart
@@ -1,83 +1,240 @@
 import 'package:festival_post_app/model/modelclass.dart';
 import 'package:festival_post_app/model/modellist.dart';
 import 'package:festival_post_app/utils/color.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:glowy_borders/glowy_borders.dart';
+
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
   @override
-
   State<Homescreen> createState() => _HomescreenState();
 }
 
 class _HomescreenState extends State<Homescreen> {
-
   @override
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    myfestivak=festivalList.map((e)=>Festival.fromMap(e)).toList();
+    myfestivak = festivalList.map((e) => Festival.fromMap(e)).toList();
   }
+
   Widget build(BuildContext context) {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor:back,
+      backgroundColor: back,
       appBar: AppBar(
-        backgroundColor:back,
-        title: Text('Fley Maker',style: TextStyle(color: Colors.white),),
+        backgroundColor: back,
+        title: Text(
+          'Fley Maker',
+          style: TextStyle(color: Colors.white),
+        ),
         actions: [
-          IconButton(onPressed: () {
-
-          }, icon: Icon(Icons.circle_notifications,color: button,size: 30,),),
-          IconButton(onPressed: () {
-
-          }, icon: Icon(Icons.grid_view_sharp,color: button,size: 30,),)
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.circle_notifications,
+              color: button,
+              size: 30,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.grid_view_sharp,
+              color: button,
+              size: 30,
+            ),
+          )
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Column(
-          children: [
-            TextField(
-cursorColor: button,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-
-                hintText: 'Search here...',hintStyle: TextStyle(color: Colors.white),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: button ,width: 1.5),
-                  borderRadius: BorderRadius.circular(15)
-                ),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15)
-                )
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                cursorColor: button,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Search here...',
+                    hintStyle: TextStyle(color: Colors.white),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: button, width: 1.5),
+                        borderRadius: BorderRadius.circular(15)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15))),
               ),
-            ),
-            SizedBox(height: h*0.040,),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+              SizedBox(
+                height: h * 0.020,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ...List.generate(myfestivak.length,(index)=>Container(
-                    height: h*0.200,
-                    width: w*0.681,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15)
-                        ,color: Colors.white,
-                        image: DecorationImage(image: AssetImage('${myfestivak[index].image},'))
-                    ),
-                  ),),
-
-                  SizedBox(width: w*0.040,),
+                  Text(
+                    'Category',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: w / 15),
+                  ),
+                  Text(
+                    'SeeAll',
+                    style: TextStyle(
+                        color: button,
+                        fontWeight: FontWeight.w500,
+                        fontSize: w / 30),
+                  ),
                 ],
               ),
-            )
-            
-          ],
+              SizedBox(
+                height: h * 0.020,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      myfestivak.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.only(right: 5, left: 5),
+                        child: AnimatedGradientBorder(
+                          gradientColors: [
+                            Colors.tealAccent,
+                            Colors.teal,
+                            Colors.blue
+                          ],
+                          borderRadius: BorderRadius.circular(999),
+                          glowSize: 0,
+                          borderSize: 1,
+                          child: CircleAvatar(
+                              radius: h / 9,
+                              backgroundImage:
+                                  AssetImage('${myfestivak[index].image}')),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: w * 0.020,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: h * 0.020,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Large Festival',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: w / 16),
+                  ),
+                  Text(
+                    'SeeAll',
+                    style: TextStyle(
+                        color: button,
+                        fontWeight: FontWeight.w500,
+                        fontSize: w / 30),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: h * 0.020,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      myfestivak.length,
+                      (index) => Stack(
+                        children: [
+                          Container(
+                            height: h * 0.250,
+                            width: w * 0.400,
+                            margin: EdgeInsets.only(left: 10, right: 10),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('${myfestivak[0].img[2]}'),
+                                    fit: BoxFit.cover)),
+                            alignment: Alignment.topRight,
+                            child: IconButton(style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.black38)),onPressed: () {
+
+                            }, icon: Icon(Icons.bookmark_add_outlined,color: Colors.white,)),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: h * 0.020,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Large Festival',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: w / 16),
+                  ),
+                  Text(
+                    'SeeAll',
+                    style: TextStyle(
+                        color: button,
+                        fontWeight: FontWeight.w500,
+                        fontSize: w / 30),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: h * 0.020,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      myfestivak.length,
+                          (index) => Stack(
+                        children: [
+                          Container(
+                            height: h * 0.250,
+                            width: w * 0.400,
+                            margin: EdgeInsets.only(left: 10, right: 10),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                    image:
+                                    AssetImage('${myfestivak[1].img[2]}'),
+                                    fit: BoxFit.cover)),
+                            alignment: Alignment.topRight,
+                            child: IconButton(style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.black38)),onPressed: () {
+
+                            }, icon: Icon(Icons.bookmark_add_outlined,color: Colors.white,)),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
